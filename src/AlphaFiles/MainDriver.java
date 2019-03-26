87a60ca683b938fc762baaf9e1233b6ed559d7c1
ezekywok <jason.ham.awesome@gmail.com>
2019-03-26 08:56:28 -0400

    yes

--- src/AlphaFiles/MainDriver.java
+++ src/AlphaFiles/MainDriver.java
@@ -1,5 +1,20 @@
 package AlphaFiles;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class MainDriver {
+    public static void main(String[]args){
+        SongAssignAlpha song1;
+        {
+            try {
+                song1 = new SongAssignAlpha();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        ArrayList<Notes> beans = song1.getTimings();
+    }
+
 
 }
--- src/AlphaFiles/SongAssignAlpha.java
+++ src/AlphaFiles/SongAssignAlpha.java
@@ -1,21 +1,39 @@
 package AlphaFiles;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
 import java.util.ArrayList;
 public class SongAssignAlpha {
     private File file = new File("src/AlphaFiles/songstuff.txt");
     private BufferedReader br = new BufferedReader(new FileReader(file));
     private String title;
     private int songlength;
-    private ArrayList<Double> timings;
+    private ArrayList<Notes> timings;
 
 
-    //default constructor (throws FileReader exception)
-    public SongAssignAlpha() throws FileNotFoundException {
+    //default constructor (throws IOException exception)
+    public SongAssignAlpha() throws IOException {
         title = br.readLine();
+        songlength = Integer.parseInt(br.readLine());
+        int counter = 0,loc = -1;
+        double times1 = -1,times2 = -1;
+        String str;
+        while((str = br.readLine()) != null){
+            if(counter%3==0){
+                loc = Integer.parseInt(str);
+            }
+            if (counter%3==1){
+                times1 = Double.parseDouble(str);
+            }
+            if(counter%3==2){
+                times2 = Double.parseDouble(str);
+                timings.add(new Notes(loc,times1,times2));
+            }
+            counter++;
+        }
+    }
+    //get methods
+    public ArrayList<Notes> getTimings(){
+        return timings;
     }
 
 }
